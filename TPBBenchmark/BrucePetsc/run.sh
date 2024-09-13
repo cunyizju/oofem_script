142d9a0d3891f6f5683be3e6f6419d8bbc1513ff
cunyizju <cunyicom@outlook.com>
2024-09-13 23:04:46 +0200

    set profileopt=true as default; TPB test pass

---
+++ TPBBenchmark/BrucePetsc/run.sh
@@ -0,0 +1,5 @@
+# /home/bruce/projects/oofem_LCY/build/oofem -f TPB_Xie.in > TPB_Xie.log &
+/home/bruce/projects/oofem_LCY/build/opt/oofem -f TPB_Xie.in -ksp_type cg -pc_type jacobi > TPB_Xie.log &
+# /home/bruce/Videos/Repo2024/oofem/build/opt/oofem -f TPB_Xie.in -ksp_type cg -pc_type jacobi > TPB_Xie.log &
+
+# python ../visual/extractor.py -f TPB_Xie.in >> ../visual/TPB_4_Petsc.csv
